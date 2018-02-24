79ae54b115fc670a8bccb2cf82e30a21f13b8529
Nikita Buyevich <nikita.buyevich@gmail.com>
2018-02-24 14:46:27 -0600

    Add pass in limit argument

--- insert_data_into_patient_survey.py
+++ insert_data_into_patient_survey.py
@@ -3,11 +3,13 @@
 
 import psycopg2
 import sys
-import pandas as pd
-from sodapy import Socrata
-import json
-from pprint import pprint
 import sqlalchemy
+import pandas as pd
+import json
+
+from sodapy import Socrata
+from pprint import pprint
+from sys import argv
 
 # Connection strings
 from db_connection import *
@@ -17,26 +19,32 @@ try:
                                                           DB_USER, DB_PASS,
                                                           DB_HOST, DB_PORT,
                                                           DB_NAME)
+
+    if len(sys.argv) > 1:
+        limit = sys.argv[1]
+    else:
+        limit = "1000"
+
     # Create Engine
     ENGINE = sqlalchemy.create_engine(
         SQLALCHEMY_DATABASE_URI, pool_size=POOL_SIZE, max_overflow=0)
 
-    print("Connecting to DB... ", end="")
+    print("Connecting to DB...", end=" ")
     # Create HTTP Client
-    client = Socrata("data.medicare.gov", "Ej5oTGjip0RAtXjHBP6IiVPtd")
+    client = Socrata("data.medicare.gov", API_KEY)
 
     print("done.")
 
-    print("Fetching data... ", end="")
+    print("Fetching data with a limit of ..." + limit, end=" ")
     # Get HCAHPS Data
-    results = client.get("rmgi-5fhi", limit=100000)
+    results = client.get("rmgi-5fhi", limit=limit)
 
     print("done.")
 
     # Convert to Pandas DataFrame
     results_df = pd.DataFrame.from_records(results)
 
-    print("Inserting data into DB... ", end="")
+    print("Inserting data into DB...", end=" ")
 
     # Insert the converted data into SQL
     results_df.to_sql(
